15b68db5a9f31e80374d4fc409793e5119f5317f
lifeadsevent-cmyk <lifeadsevent@gmail.com>
2026-01-19 15:03:16 +0100

    refactor: Update component names and scripts
    Update component filenames and imports to use PascalCase. Adjust package.json scripts by removing the unnecessary "start" script.

--- App.tsx
+++ App.tsx
@@ -2,10 +2,10 @@
 import React, { useState, useMemo } from 'react';
 import { RAW_PLAYER_DATA, ALLIANCE_AVERAGES } from './data.ts';
 import { PlayerData, SortKey, SortOrder } from './types.ts';
-import Statstable from './components/statstable.tsx';
-import DashboardCards from './components/dashboardcards.tsx';
-import PerformanceChart from './components/performancechart.tsx';
-import UnitsView from './components/unitsview.tsx';
+import StatsTable from './components/StatsTable.tsx';
+import DashboardCards from './components/DashboardCards.tsx';
+import PerformanceChart from './components/PerformanceChart.tsx';
+import UnitsView from './components/UnitsView.tsx';
 
 /**
  * Main Application Component
